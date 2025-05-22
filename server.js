f5b089489ab5b1ad420ef6c3dbd208554a820506
Mkrm12 <muhammedmukarramnazir@yahoo.co.uk>
2025-05-22 20:48:50 +0100

    Updated portfolio details with SBOT and poster integration

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var path = require('path');  // <-- Add this line
-
+                                                                                                                                                                               
 // Create the express application object
 const app = express();
 const port = 8000;
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files from the "iPortfolio" folder, including assets (css, js, etc.)
-app.use('/portfolio', express.static(path.join(__dirname, 'iPortfolio')));
-
-// Route for the /portfolio homepage (index.html in the "iPortfolio" folder)
-app.get('/portfolio', (req, res) => {
-  res.sendFile(path.join(__dirname, 'iPortfolio', 'index.html'));
-});
-
+app.use(bodyParser.urlencoded({ extended: true }));                                                                                                                            
+                                                                                                                                                                               
+app.use('/', express.static(path.join(__dirname, 'iPortfolio'))); 
+                                                                                                                                                                               
+app.get('/', (req, res) => {   res.sendFile(path.join(__dirname, 'iPortfolio', 'index.html')); });
+                                                                                                                                                                               
 // Start the server
-app.listen(port, () => {
+app.listen(port, () => {                                                                                                                                                       
   console.log(`Server is running at http://localhost:${port}`);
-});
+});                                                                                                                                                                            
+                                                                                                                                                                               
+                                                                                                                                                                               
+                                 
